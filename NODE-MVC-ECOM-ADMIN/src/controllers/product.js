e8e488ce1ab254e5c22edd353b1bc179cfe6c576
Ainish Vadhwani <ainishvadhwani24@gmail.com>
2025-01-29 02:54:14 +0530

    Basic Validation on Server Side

--- NODE-MVC-ECOM-ADMIN/src/controllers/product.js
+++ NODE-MVC-ECOM-ADMIN/src/controllers/product.js
@@ -11,14 +11,43 @@ const fetchProducts=(req, res)=>{
     res.json(products)
 }
 const createProduct=(req,res)=>{
-    const newProduct=req.body
+    const products = getP()
+    const {title,currentPrice,mrp,imgURL}=req.body
+
+    //Validation Rules
+    const errors=[]
+
+    if(title=='' || title.length<3){
+        errors.push("Invalid title entered")
+    }
+
+    if(currentPrice=='' || isNaN(Number(currentPrice))){
+        errors.push("Invalid current price entered")
+    }
+
+    if(mrp=='' || isNaN(Number(mrp))){
+        errors.push("Invalid MRP entered")
+    }
+
+    try{
+        const url1 = new URL(imgURL)
+    }catch(error){
+        errors.push("Invalid image URL entered")
+    }
+
+    if(errors.length!=0){
+        res.render('dashboard',{products, errorMessage:errors[0]})
+    }
+
+    newProduct={title,currentPrice,mrp,imgURL}
     createP(newProduct)
     res.redirect("/dashboard")
 }
 const displayDashboard=(req,res)=>{
     const products=getP()
     res.render('dashboard',{
-        products:products
+        products:products,
+        errorMessage:""
     })
 }
 const updateProduct=(req,res)=>{
