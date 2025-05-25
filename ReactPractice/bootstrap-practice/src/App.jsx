18521b0714d7f53fcd328e8b31d95f0d74db5646
Ainish Vadhwani <ainishvadhwani24@gmail.com>
2025-05-25 12:17:14 +0530

    Learning Bootstrap-React

---
+++ ReactPractice/bootstrap-practice/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>React Bootstrap</h1>
+        <Button variant="danger">Click Here</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
